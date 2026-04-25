d4683fc9f235884a1ea5cf30db29cd4136763646
Linnette Arellano <132861970+linnettearel@users.noreply.github.com>
2026-04-25 03:30:50 -0700

    Add files via upload
    attempt #1

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+DEVELOPER(S): <your name(s)>
+COLLABORATORS: <anyone who helped you>
+DATE: <date last worked on>
+'''
+
+"""
+A one line summary of the program, terminated by a period.
+
+Leave one blank line.  The rest of this docstring should contain an
+overall description of the program.
+"""
+
+##########################################
+# IMPORTS:
+# <list of modules needed for the program and their purpose>
+##########################################
+# <replace this line with import statement(s)>
+
+
+##########################################
+# FUNCTIONS:
+##########################################
+# <replace this line with function definitions, each needs a description>
+
+
+##########################################
+# MAIN PROGRAM:
+##########################################
+# <replace this line with your main program>
